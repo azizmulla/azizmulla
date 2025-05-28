0a53551fa5756fe1593112e6b41f56dddebb7d62
azizmulla <azzizzmulla@gmail.com>
2025-05-28 18:48:17 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 a = 1
 b = 2
-if(a  > 3):
-  print("yes you are right")
+if(a  < 3):
+  print("a is grater then 3")
 else:
-  print("noo! you are wrong")
+  print("a is smaller then 3")
 
 
 print("hello buddy")
